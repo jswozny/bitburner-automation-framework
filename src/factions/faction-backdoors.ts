ddf741cd2a40fea54b07f147a95cc3aee7a8e6b7
Shawn Squire <shawn@shawnsquire.me>
2026-02-01 15:05:54 -0500

    Fixed faction backdoor logic

--- src/factions/faction-backdoors.ts
+++ src/factions/faction-backdoors.ts
@@ -20,10 +20,16 @@ export async function main(ns: NS): Promise<void> {
 
         const { parentByHost } = discoverAllWithDepthAndPath(ns, start, 100);
         const path = pathToArray(parentByHost, host).slice(1);
+
+        ns.tprint(`Backdooring ${host}`);
+        ns.tprint(pathTo(parentByHost, host));
         for (const host of path) {
             ns.singularity.connect(host);
+        }
+        while (!ns.getServer(host).backdoorInstalled) {
             await ns.singularity.installBackdoor();
         }
+        ns.tprint("Installed!")
         ns.singularity.connect("home");
     }
 }
