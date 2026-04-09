a45385ed93fcb5f45ab2eb6d494d1ecaea1160c2
Jeremy Wozny <61649767+jswozny@users.noreply.github.com>
2026-04-09 15:10:28 -0400

    revert SF commits
    Remove check for SF4.1 requirement before purchasing NeuroFlux Governor.

--- src/actions/purchase-neuroflux.ts
+++ src/actions/purchase-neuroflux.ts
@@ -17,10 +17,7 @@ export const MANUAL_COMMAND = 'ns.singularity.purchaseAugmentation("FACTION", "N
 
 export async function main(ns: NS): Promise<void> {
   ns.disableLog("ALL");
- if (!ns.getResetInfo().ownedSF.has(4)) {
-  ns.print("Error: SF4.1 is required to purchase NeuroFlux Governor. You do not have SF4.1 unlocked.");
-  return;
- }
+
   const flags = ns.flags([
     ["faction", ""],
     ["dry-run", false],
